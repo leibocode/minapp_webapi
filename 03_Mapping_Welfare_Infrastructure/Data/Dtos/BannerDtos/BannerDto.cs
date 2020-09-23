a0edadc2871838fba8bdf36121276c2237b84832
Leibo <2525217584@qq.com>
2020-09-23 13:43:14 +0800

    update Banner Dto

---
+++ 03_Mapping_Welfare_Infrastructure/Data/Dtos/BannerDtos/BannerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_Mapping_Welfare_Infrastructure.Data.Dtos.BannerDtos
+{
+    public class BannerDto
+    {
+        public Guid Id { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ 03_Mapping_Welfare_Infrastructure/Data/EntityConfigurations/BannerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _02_Mapping_Welfare_Domain.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _03_Mapping_Welfare_Infrastructure.Data.EntityConfigurations
+{
+    class BannerConfiguration : IEntityTypeConfiguration<Banner>
+    {
+        public void Configure(EntityTypeBuilder<Banner> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
